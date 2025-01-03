4196656e2c5d8d92c023bca5baa9c53f7c35dff4
Brian Haas <bhaas@broadinstitute.org>
2025-01-03 15:14:56 +0000

    add

---
+++ sequin_analysis/__LRAA/mixB/run_LRAA_quant.sh
@@ -0,0 +1,1 @@
+/home/bhaas/GITHUB/MDL/LongReadAlignmentAssembler/LRAA --LowFi --quant_only --bam mixB_ont.chrIS.SS.bam --genome ../../reference_data/sequin.fasta --gtf ../../reference_data/sequin_annotations.gtf
